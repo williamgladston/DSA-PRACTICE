c7b31bf65f7eac73ffdf30c55153afe4c9a96c15
williamgladston <williamgladston4@gmail.com>
2025-10-03 19:56:53 +0530

    Bitwise operator

---
+++ Operators/bitwis_operator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a ,b ;
+    a = 5;
+    b = 6;
+
+    cout<< (a|b) <<endl;
+    cout<< (a&b) <<endl;
+    cout<< (a^b) <<endl;
+    cout<< (~a) <<endl;
+    cout<< (15<<1) <<endl;
+    cout<< (15<<2) <<endl;
+    cout<< (15>>1) <<endl;
+    cout<< (15>>2) <<endl;
+}
